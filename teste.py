7fc0fda4cc0e4cc63135724c6e74929b3473ff86
Eduardo Melo Glaser <eduardomeloglaser@gmail.com>
2026-08-13 19:23:34 -0300

    test(ap01-git): testa divisao

--- teste.py
+++ teste.py
@@ -5,3 +5,4 @@ b = 2
 print(f'{a} + {b} = {calculadora.soma}')
 print(f'{a} - {b} = {calculadora.subtracao}')
 print(f'{a} * {b} = {calculadora.multiplicacao}')
+print(f'{a} / {b} = {calculadora.divisao}')
